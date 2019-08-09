496e8610665dc917f6b02ec0af25f92a5086a6fa
kyle_wu <kyle.wuhao@alibaba-inc.com>
2019-08-09 19:18:36 +0800

    修复编译问题

--- flutter_douban/lib/main.dart
+++ flutter_douban/lib/main.dart
@@ -21,7 +21,6 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: S.of(context).app_name,
       theme: ThemeData(
         // This is the theme of your application.
         //
